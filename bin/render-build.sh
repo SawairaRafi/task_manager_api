af7be88d1f85952c16ca25d92f2c12c116e953a8
sawairarafi <sawairarafi@gmail.com>
2024-12-23 16:05:18 +0500

    added render files

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash# exit on error
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean up assets
 bundle exec rails assets:clean
